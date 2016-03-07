d29ea27e421a033bf068cd5acb72bb9113f0456e
nasseralkmim <nasser.alkmim@gmail.com>
2016-03-07 20:05:01 -0300

    edited setup.py file

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(name='sapy',
       author='Nasser Alkmim',
       author_email='nasser.alkmim@gmail.com',
       url='https://github.com/nasseralkmim/sapy',
+      download_url='https://github.com/nasseralkmim/sapy/releases/tag/0.1',
       license='MIT',
       packages=['sapy'],
       install_requires=[
