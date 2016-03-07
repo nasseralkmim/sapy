4aaab06545654832ddc40bfd70af8ccc13f5c6fb
nasseralkmim <nasser.alkmim@gmail.com>
2016-03-07 19:59:48 -0300

    edited setup.py

--- setup.py
+++ setup.py
@@ -3,15 +3,12 @@ from setuptools import setup
 setup(name='sapy',
       version='0.1',
       description='matrix structural analysis',
-      url='nasseralkmim.github.io',
       author='Nasser Alkmim',
       author_email='nasser.alkmim@gmail.com',
+      url='https://github.com/nasseralkmim/sapy',
       license='MIT',
       packages=['sapy'],
       install_requires=[
           'numpy',
-          'matplotlib',
-          'networkx',
-          'scipy'
-      ],
+          'matplotlib'],
       zip_safe=False)
